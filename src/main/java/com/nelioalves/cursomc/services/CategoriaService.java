5080b2507686de070b4111a95f594ac215d027ae
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-04-08 22:51:53 -0300

    Atualizando categoria com PUT

--- src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
+++ src/main/java/com/nelioalves/cursomc/services/CategoriaService.java
@@ -30,6 +30,11 @@ public class CategoriaService {
 		obj.setId(null);
 		return categoriaRepository.save(obj);
 	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return categoriaRepository.save(obj);
+	}
 	
 	
 }
