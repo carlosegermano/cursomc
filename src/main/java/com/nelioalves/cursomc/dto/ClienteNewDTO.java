283e094fea7cb1ccecde9abc62a8c05987e9c67e
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-04-14 00:27:12 -0300

    Adicionando senha a cliente

--- src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/nelioalves/cursomc/dto/ClienteNewDTO.java
@@ -27,6 +27,9 @@ public class ClienteNewDTO implements Serializable {
 	
 	private Integer tipo;
 	
+	@NotEmpty(message = "Preenchimento obrigatório!")
+	private String senha;
+	
 	// Endereco
 	@NotEmpty(message = "Preenchimento obrigatório!")
 	private String logradouro;
@@ -83,6 +86,14 @@ public class ClienteNewDTO implements Serializable {
 	
 	public void setTipo(Integer tipo) {
 		this.tipo = tipo;
+	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
 	}
 	
 	public String getLogradouro() {
