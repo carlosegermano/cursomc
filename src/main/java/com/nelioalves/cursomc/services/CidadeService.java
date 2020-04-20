6e41def19e4e40fa5bc0d0b6509a1beacf914897
Carlos Eduardo <carlos.germano@dce.ufpb.br>
2020-04-20 15:37:51 -0300

    Endpoints para buscar estados e cidades

---
+++ src/main/java/com/nelioalves/cursomc/services/CidadeService.java
@@ -0,0 +1,20 @@
+package com.nelioalves.cursomc.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.nelioalves.cursomc.domain.Cidade;
+import com.nelioalves.cursomc.repository.CidadeRepository;
+
+@Service
+public class CidadeService {
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	public List<Cidade> findByEstado(Integer estado_id){
+		return cidadeRepository.findCidades(estado_id);
+	}
+}
